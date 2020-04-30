4582e610873e13becc09664beba479c3bf68e3b0
RRRR <rafael.ramosr@hotmail.com>
2020-04-30 08:22:59 -0500

    Widgets api twitter y api map agregadados

--- views/widgets/mapa.php
+++ views/widgets/mapa.php
@@ -1,30 +1,23 @@
-<style>
-	#map {
-		height: 100%;
-	}
-</style>
+<div id="mapid" style="height: 100%;"></div>
 
-<img src="https://maps.googleapis.com/maps/api/staticmap?zoom=15&size=100x200&maptype=roadmap&key=AIzaSyBeS0_l--kJyOVbbC3SLrLrP1SkEfCepbQ&markers=color:blue%7Clabel:Y%7C19.4326077,-99.13320799999997">
-
-<!--<div id="map"></div>-->
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" />
+<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
 <script>
-	var map;
-	function initMap() {
-		var myLatLng = { lat: 19.4326077, lng: -99.13320799999997 };
-		map = new google.maps.Map(document.getElementById('map'), {
-			center: myLatLng,
-			zoom: 6
-		});
+	// Coordenadas y zoom
+	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-		var marker = new google.maps.Marker({
-			position: { lat: 19.4326177, lng: -99.23320799999997 },
-			map: map,
-			title: "Oficinas"
-		});
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
+	}).addTo(mymap);
 
-	}
+	L.marker([51.5, -0.09]).addTo(mymap).bindPopup("<b>Bienvenido!</b><br />Puedes encontrarnos aquí.").openPopup();
 
-
+	var popup = L.popup();
 
 </script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBeS0_l--kJyOVbbC3SLrLrP1SkEfCepbQ&callback=initMap" async defer></script>
